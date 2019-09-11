9424aaf403644ead8a64b747f293eb7689b73eb8
Ayanna Burrowes <a.burrowes529@gmail.com>
2019-09-10 23:44:12 -0400

    fixing errors in database

---
+++ database/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO notes (title, body) VALUES ("Audition Dance", "Prepare audition dance for new members on September 17"),
+INSERT INTO notes (title, body) VALUES("Confirm Appointment", "Call Dentist Office"),
+INSERT INTO notes (title, body) VALUES("Visit Family", "Visit dad and siblings on Wednesday"),
+  
